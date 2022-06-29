a591a4376568c73ac3e7d7f2991809a4f17c8275
rraammg <rraammg@gmail.com>
2022-06-29 22:57:23 +0530

    first commit

---
+++ src/webDriver/A1_WebDriver.java
@@ -0,0 +1,14 @@
+package webDriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class A1_WebDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\eclipse-workspace\\C1_WebDriver\\browserdrive\\chromedriver.exe");
+	WebDriver WD=new ChromeDriver();
+	}
+
+}
